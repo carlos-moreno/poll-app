456a19f995daf42e5e65c12fd8743f4e869eba5e
Carlos Moreno <omorenodovale@gmail.com>
2020-06-06 22:27:42 -0500

    Add tests for model Choice

---
+++ poll/core/tests/test_model_choice.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from django.utils import timezone
+
+from poll.core.models import Question, Choice
+
+
+class ChoiceTest(TestCase):
+    def setUp(self) -> None:
+        self.question = Question.objects.create(
+            question_text="What's up?",
+            pub_date=timezone.now()
+        )
+        self.choice = Choice.objects.create(
+            question=self.question,
+            choice_text="Just hacking again.",
+            votes=0
+        )
+
+    def test_str(self):
+        self.assertEqual("Just hacking again.", str(self.choice))
